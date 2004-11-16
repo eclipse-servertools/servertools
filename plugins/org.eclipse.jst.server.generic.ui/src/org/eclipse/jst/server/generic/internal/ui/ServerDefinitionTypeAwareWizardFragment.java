3deab4722cc8f72f4e9c73c4ea1bc77e300f1bf4
gercan <gercan>
2004-11-16 17:21:48 +0000

    validaton, title and description

--- plugins/org.eclipse.jst.server.generic.ui/src/org/eclipse/jst/server/generic/internal/ui/ServerDefinitionTypeAwareWizardFragment.java
+++ plugins/org.eclipse.jst.server.generic.ui/src/org/eclipse/jst/server/generic/internal/ui/ServerDefinitionTypeAwareWizardFragment.java
@@ -7,9 +7,9 @@
 package org.eclipse.jst.server.generic.internal.ui;
 
 import java.util.Map;
-
 import org.eclipse.jst.server.generic.core.CorePlugin;
 import org.eclipse.jst.server.generic.internal.xml.ServerTypeDefinition;
+import org.eclipse.jst.server.generic.ui.GenericUiPlugin;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -24,22 +24,42 @@ import org.eclipse.wst.server.ui.wizard.WizardFragment;
  */
 public abstract class ServerDefinitionTypeAwareWizardFragment extends WizardFragment {
 
+    private IWizardHandle fWizard;
 	public boolean hasComposite() {
 		return true;
 	}
 
 	public Composite createComposite(Composite parent, IWizardHandle handle) {
 	    
+	    this.fWizard = handle;
 	    Composite container = new Composite(parent, SWT.NONE);
 		container.setLayoutData(new GridData(GridData.FILL_BOTH));
 		GridLayout grid = new GridLayout(1,false);
 		container.setLayout(grid);
+		handle.setImageDescriptor(GenericUiPlugin.getDefault().imageDescriptor(GenericUiPlugin.WIZBAN_IMAGE));
+		handle.setTitle(title());
+		handle.setDescription(description());
 		createContent(container,handle);
 		return container;
 	}
-
+	public IWizardHandle getWizard(){
+	    return fWizard;
+	}
+	/**
+	 * Returns the description to be displayed on the wizard head.
+	 * @return
+	 */
+	public abstract String description();
+	/**
+	 * Returns the title of the wizard.
+	 * @return
+	 */
+	public abstract String title();
+	
 	public abstract void createContent(Composite parent, IWizardHandle handle);
 
+	public abstract void serverDefinitionTypePropertiesChanged();
+	
 	protected ServerTypeDefinition getServerTypeDefinition(String definitionID, Map properties)
 	{
 	    return CorePlugin.getDefault().getServerTypeDefinitionManager().getServerRuntimeDefinition(definitionID,properties);
