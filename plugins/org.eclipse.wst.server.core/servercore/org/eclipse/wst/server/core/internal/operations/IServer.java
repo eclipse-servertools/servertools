f68d20f3c13a4cdae2bba1f8ae84e09246e659e5
deboer <deboer>
2004-11-23 18:05:32 +0000

    *** empty log message ***

---
+++ plugins/org.eclipse.wst.server.core/servercore/org/eclipse/wst/server/core/internal/operations/IServer.java
@@ -0,0 +1,26 @@
+/**********************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Contributors:
+ *    IBM - Initial API and implementation
+ **********************************************************************/
+package org.eclipse.wst.server.core.internal.operations;
+
+import org.eclipse.core.runtime.IStatus;
+/**
+ * A server.
+ */
+public interface IServer {
+	/**
+	 * 
+	 * @param operation
+	 * @param state
+	 * @param synchronous
+	 * @return
+	 */
+	public IStatus operate(IServerOperation[] operation, ServerState state, boolean synchronous);
+}
