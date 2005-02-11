5abf1871c2415cb320e6df956caaae6e341ab3d3
deboer <deboer>
2005-02-11 21:47:10 +0000

    [84991] Use monitor preference page for properties

--- tests/org.eclipse.wst.internet.monitor.ui.tests/src/org/eclipse/wst/internet/monitor/ui/tests/DialogsTestCase.java
+++ tests/org.eclipse.wst.internet.monitor.ui.tests/src/org/eclipse/wst/internet/monitor/ui/tests/DialogsTestCase.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2004 IBM Corporation and others.
+ * Copyright (c) 2005 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,17 +13,11 @@ package org.eclipse.wst.internet.monitor.ui.tests;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.wst.internet.monitor.ui.internal.MonitorDialog;
-import org.eclipse.wst.internet.monitor.ui.internal.MonitorPreferencesDialog;
 import junit.framework.TestCase;
 
 public class DialogsTestCase extends TestCase {
 	private Shell getShell() {
 		return PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-	}
-
-	public void testMonitorPreferencesDialog() {
-		MonitorPreferencesDialog dsd = new MonitorPreferencesDialog(getShell());
-		UITestHelper.assertDialog(dsd);
 	}
 
 	public void testMonitorDialog() {
