5a0d3c6b03d8113d3866207c32f3d5100bb46558
deboer <deboer>
2005-04-20 19:26:26 +0000

    *** empty log message ***

--- plugins/org.eclipse.jst.server.tomcat.ui/tomcatui/org/eclipse/jst/server/tomcat/ui/internal/TomcatLaunchConfigurationTabGroup.java
+++ plugins/org.eclipse.jst.server.tomcat.ui/tomcatui/org/eclipse/jst/server/tomcat/ui/internal/TomcatLaunchConfigurationTabGroup.java
@@ -10,18 +10,14 @@
  *******************************************************************************/
 package org.eclipse.jst.server.tomcat.ui.internal;
 
-import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
-import org.eclipse.debug.ui.CommonTab;
-import org.eclipse.debug.ui.EnvironmentTab;
-import org.eclipse.debug.ui.ILaunchConfigurationDialog;
-import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.debug.ui.*;
 import org.eclipse.debug.ui.sourcelookup.SourceLookupTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaClasspathTab;
 
 import org.eclipse.wst.server.ui.ServerLaunchConfigurationTab;
 /**
- * 
+ * A debug tab group for launching Tomcat. 
  */
 public class TomcatLaunchConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
 	/*
