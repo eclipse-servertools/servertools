a0c27048b63c3bc31f8afa829a3df99e6fdb7e30
jeffliu <jeffliu>
2005-05-24 17:47:27 +0000

    [96475] Check in API test for ServerUIUtil.showNewRuntimeWizard(...)

--- tests/org.eclipse.wst.server.ui.tests/src/org/eclipse/wst/server/ui/tests/ServerUIUtilTestCase.java
+++ tests/org.eclipse.wst.server.ui.tests/src/org/eclipse/wst/server/ui/tests/ServerUIUtilTestCase.java
@@ -11,9 +11,27 @@
 package org.eclipse.wst.server.ui.tests;
 
 import junit.framework.TestCase;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.wst.server.ui.ServerUIUtil;
 
 public class ServerUIUtilTestCase extends TestCase {
 	public void testShowNewRuntimeWizard() {
-		//ServerUIUtil.showNewRuntimeWizard(null, null, null);
+		final Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		final Display display = shell.getDisplay();
+		shell.getDisplay().asyncExec(new Runnable()
+		{
+			public void run()
+			{
+				ServerUIUtil.showNewRuntimeWizard(shell, null, null);
+			}
+		});
+		while (!display.readAndDispatch())
+		{
+			// wait until the UI thread settles (ie. the wizard opens)
+			// then close the active window, which should be the wizard
+			PlatformUI.getWorkbench().getActiveWorkbenchWindow().close();
+		}
 	}
 }
