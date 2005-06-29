0df80d378b5605572fb19067caef94689f386e55
deboer <deboer>
2005-06-29 21:55:09 +0000

    [94617] Initial steps for assembly builder

---
+++ plugins/org.eclipse.wst.server.core/servercore/org/eclipse/wst/server/core/internal/AssemblyBuilder.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.server.core.internal;
+
+import java.util.Map;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+/**
+ * Server assembly builder.
+ */
+public class AssemblyBuilder extends IncrementalProjectBuilder {
+
+	protected IProject[] build(int kind, Map args, IProgressMonitor monitor) throws CoreException {
+		return null;
+	}
+}
