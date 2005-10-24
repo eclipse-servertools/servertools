bcb2628f48ce9f0b5b61e6d8c468dfcc59428d45
gercan <gercan>
2005-10-24 18:44:21 +0000

    adjust to renaming of ant publisher in generic core

--- plugins/org.eclipse.jst.server.websphere.core/src/org/eclipse/jst/server/websphere/core/AntPublisher.java
+++ plugins/org.eclipse.jst.server.websphere.core/src/org/eclipse/jst/server/websphere/core/AntPublisher.java
@@ -3,7 +3,7 @@ package org.eclipse.jst.server.websphere.core;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 
-public class AntPublisher extends org.eclipse.jst.server.generic.core.internal.AntPublisher {
+public class AntPublisher extends org.eclipse.jst.server.generic.core.internal.publishers.AntPublisher {
 	public static final String PUBLISHER_ID="org.eclipse.jst.server.generic.websphere.antpublisher"; //$NON-NLS-1$
 
 	protected void setupAntLaunchConfiguration(ILaunchConfigurationWorkingCopy wc) {
