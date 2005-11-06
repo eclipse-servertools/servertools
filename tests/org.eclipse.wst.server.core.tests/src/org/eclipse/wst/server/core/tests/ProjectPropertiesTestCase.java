1ba2e0d9280148d074c617e3411cc13621b05a3f
deboer <deboer>
2005-11-06 03:53:44 +0000

    [110455] Installable runtimes

--- tests/org.eclipse.wst.server.core.tests/src/org/eclipse/wst/server/core/tests/ProjectPropertiesTestCase.java
+++ tests/org.eclipse.wst.server.core.tests/src/org/eclipse/wst/server/core/tests/ProjectPropertiesTestCase.java
@@ -16,25 +16,11 @@ import junit.framework.TestCase;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.wst.server.core.*;
-import org.eclipse.wst.server.core.internal.IProjectPropertiesListener;
 import org.eclipse.wst.server.core.internal.ProjectProperties;
 
 public class ProjectPropertiesTestCase extends TestCase {
 	protected static IProject project;
 	protected static IProjectProperties props;
-	
-	protected static IProject projectEvent;
-	protected static IServer serverEvent;
-	protected static IRuntime runtimeEvent;
-	protected static int count;
-
-	protected IProjectPropertiesListener listener = new IProjectPropertiesListener() {
-		public void runtimeTargetChanged(IProject project2, IRuntime runtime) {
-			projectEvent = project2;
-			runtimeEvent = runtime;
-			count++;
-		}
-	};
 
 	public static Test suite() {
 		return new OrderedTestSuite(ProjectPropertiesTestCase.class, "AbstractServerTestCase");
@@ -49,10 +35,6 @@ public class ProjectPropertiesTestCase extends TestCase {
 		props = ServerCore.getProjectProperties(project);
 	}
 
-	public void test01AddListener() throws Exception {
-		((ProjectProperties)props).addProjectPropertiesListener(listener);
-	}
-
 	public void test03GetRuntime() throws Exception {
 		assertNull(props.getRuntimeTarget());
 	}
@@ -60,27 +42,9 @@ public class ProjectPropertiesTestCase extends TestCase {
 	public void test04GetServerProject() throws Exception {
 		assertFalse(((ProjectProperties) props).isServerProject());
 	}
-	
-	public void test05TestListener() throws Exception {
-		assertTrue(count == 0);
-	}
-	
-	public void test07TestListener() throws Exception {
-		// no event since we didn't change it
-		assertTrue(count == 0);
-		count = 0;
-		serverEvent = null;
-	}
 
 	public void test08SetRuntime() throws Exception {
 		props.setRuntimeTarget(null, null);
-	}
-	
-	public void test09TestListener() throws Exception {
-		// no event again
-		assertTrue(count == 0);
-		count = 0;
-		runtimeEvent = null;
 	}
 
 	public void test10SetServerProject() throws Exception {
@@ -95,25 +59,7 @@ public class ProjectPropertiesTestCase extends TestCase {
 		assertFalse(pp.isServerProject());
 	}
 
-	public void test12TestListener() throws Exception {
-		assertTrue(count == 0);
-	}
-	
-	public void test13RemoveListener() throws Exception {
-		((ProjectProperties)props).removeProjectPropertiesListener(listener);
-	}
-
 	public void test14End() throws Exception {
 		project.delete(true, true, null);
-	}
-	
-	public void test15CheckListener() throws Exception {
-		IProjectPropertiesListener listener2 = new IProjectPropertiesListener() {
-			public void runtimeTargetChanged(IProject project2, IRuntime runtime) {
-				// ignore
-			}
-		};
-		
-		listener2.runtimeTargetChanged(null, null);
 	}
 }
