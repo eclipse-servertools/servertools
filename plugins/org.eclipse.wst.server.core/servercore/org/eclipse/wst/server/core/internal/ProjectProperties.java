62ccec5231e7b9a425977789049c1188f90f23f1
deboer <deboer>
2005-11-27 21:55:57 +0000

    [117906] Server project not shareable

--- plugins/org.eclipse.wst.server.core/servercore/org/eclipse/wst/server/core/internal/ProjectProperties.java
+++ plugins/org.eclipse.wst.server.core/servercore/org/eclipse/wst/server/core/internal/ProjectProperties.java
@@ -10,7 +10,11 @@
  *******************************************************************************/
 package org.eclipse.wst.server.core.internal;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.Date;
+import java.util.Properties;
 
 import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.*;
@@ -23,11 +27,12 @@ import org.eclipse.wst.server.core.*;
  * and have a resource listener.
  */
 public class ProjectProperties implements IProjectProperties {
-	private static final String PROJECT_PREFERENCE_FILE = ".runtime";
+	private static final String PREFERENCE_FOLDER = ".settings";
+	private static final String PREFERENCE_FILE = "org.eclipse.wst.server.core.prefs";
+	private static final String SERVER_PROJECT_PREF = "org.eclipse.wst.server.core.isServerProject";
 
 	protected IProject project;
 
-	protected String runtimeId;
 	protected boolean serverProject = false;
 
 	/**
@@ -39,24 +44,27 @@ public class ProjectProperties implements IProjectProperties {
 		super();
 		this.project = project;
 	}
-	
+
 	/**
 	 * Load the preferences.
 	 */
 	private void loadPreferences() {
 		Trace.trace(Trace.FINEST, "Loading project preferences: " + project);
 		
+		if (!project.isAccessible())
+			return;
+		
 		InputStream in = null;
 		try {
-			IPath path = project.getWorkingLocation(ServerPlugin.PLUGIN_ID).append(PROJECT_PREFERENCE_FILE);
-			
-			IMemento memento = XMLMemento.loadMemento(path.toOSString());
-			
-			if (memento == null)
+			IFile file = project.getFolder(PREFERENCE_FOLDER).getFile(PREFERENCE_FILE);
+			if (file == null || !file.isAccessible())
 				return;
 			
-			runtimeId = memento.getString("runtime-id");
-			String s = memento.getString("servers");
+			in = file.getContents();
+			Properties p = new Properties();
+			p.load(in);
+			
+			String s = p.getProperty(SERVER_PROJECT_PREF);
 			if (s != null && "true".equals(s))
 				serverProject = true;
 			else
@@ -72,47 +80,51 @@ public class ProjectProperties implements IProjectProperties {
 			}
 		}
 	}
-	
+
 	private void savePreferences(IProgressMonitor monitor) throws CoreException {
-		if (project.exists() && project.isOpen()) {
-			
-			IPath path = project.getWorkingLocation(ServerPlugin.PLUGIN_ID).append(PROJECT_PREFERENCE_FILE);
-			
-			InputStream in = null;
-			try {
-				XMLMemento memento = XMLMemento.createWriteRoot("runtime");
-
-				if (runtimeId != null)
-					memento.putString("runtime-id", runtimeId);
-				if (serverProject)
-					memento.putString("servers", "true");
-				else
-					memento.putString("servers", "false");
-				in = memento.getInputStream();
-				
-				memento.saveToFile(path.toOSString());
-			} catch (Exception e) {
-				throw new CoreException(new Status(IStatus.ERROR, ServerPlugin.PLUGIN_ID, 0, "", e));
-			} finally {
-				try {
-					in.close();
-				} catch (Exception e) {
-					// ignore
-				}
-			}
+		if (project == null || !project.isAccessible())
 			return;
+		
+		IFolder folder = project.getFolder(PREFERENCE_FOLDER);
+		if (!folder.exists())
+			folder.create(true, true, monitor);
+		
+		IFile file = project.getFolder(PREFERENCE_FOLDER).getFile(PREFERENCE_FILE);
+		
+		if (file.exists() && file.isReadOnly()) {
+			IStatus status = ResourcesPlugin.getWorkspace().validateEdit(new IFile[] { file }, null);
+			if (status.getSeverity() == IStatus.ERROR)
+				// didn't work or not under source control
+				throw new CoreException(status);
 		}
-	}
-
-	protected String getRuntimeTargetId() {
-		loadPreferences();
-		return runtimeId;
-	}
-
-	protected void setRuntimeTargetId(String newRuntimeId, IProgressMonitor monitor) throws CoreException {
-		loadPreferences();
-		runtimeId = newRuntimeId;
-		savePreferences(monitor);
+		
+		Properties p = new Properties();
+		p.put("eclipse.preferences.version", "1");
+		if (serverProject)
+			p.put(SERVER_PROJECT_PREF, "true");
+		else
+			p.put(SERVER_PROJECT_PREF, "false");
+		
+		InputStream in = null;
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			p.store(out, null);
+		
+			in = new ByteArrayInputStream(out.toByteArray());
+			
+			if (file.exists())
+				file.setContents(in, true, true, monitor);
+			else
+				file.create(in, true, monitor);
+		} catch (Exception e) {
+			throw new CoreException(new Status(IStatus.ERROR, ServerPlugin.PLUGIN_ID, 0, "", e));
+		} finally {
+			try {
+				in.close();
+			} catch (Exception e) {
+				// ignore
+			}
+		}
 	}
 
 	/**
@@ -121,76 +133,7 @@ public class ProjectProperties implements IProjectProperties {
 	 * @return the runtime target
 	 */
 	public IRuntime getRuntimeTarget() {
-		loadPreferences();
-		if (runtimeId == null)
-			return null;
-		return ServerCore.findRuntime(runtimeId);
-	}
-
-	/**
-	 * Sets the runtime target for the project.
-	 * 
-	 * @param runtime the target runtime
-	 * @param monitor a progress monitor
-	 * @throws CoreException if anything goes wrong
-	 */
-	protected void setRuntimeTarget(IRuntime runtime, IProgressMonitor monitor) throws CoreException {
-		loadPreferences();
-		IRuntime oldRuntime = null;
-		if (runtimeId != null)
-			oldRuntime = ServerCore.findRuntime(runtimeId);
-		setRuntimeTarget(oldRuntime, runtime, true, monitor);
-	}
-
-	protected void setRuntimeTarget(IRuntime oldRuntime, IRuntime newRuntime, boolean save, IProgressMonitor monitor) throws CoreException {
-		Trace.trace(Trace.RUNTIME_TARGET, "setRuntimeTarget : " + oldRuntime + " -> " + newRuntime);
-		
-		if (oldRuntime == null && newRuntime == null)
-			return;
-		if (oldRuntime != null && oldRuntime.equals(newRuntime))
-			return;
-		
-		IRuntimeTargetHandler[] handlers = ServerCore.getRuntimeTargetHandlers();
-		if (handlers == null)
-			return;
-	
-		int size = handlers.length;
-		// remove old target
-		if (oldRuntime != null) {
-			IRuntimeType runtimeType = oldRuntime.getRuntimeType();
-			for (int i = 0; i < size; i++) {
-				IRuntimeTargetHandler handler = handlers[i];
-				long time = System.currentTimeMillis();
-				boolean supports = handler.supportsRuntimeType(runtimeType);
-				Trace.trace(Trace.RUNTIME_TARGET, "  < " + handler + " " + supports);
-				if (supports)
-					((RuntimeTargetHandler)handler).removeRuntimeTarget(project, oldRuntime, monitor);
-				Trace.trace(Trace.PERFORMANCE, "Runtime target: <" + (System.currentTimeMillis() - time) + "> " + handler.getId());
-			}
-		}
-		
-		// add new target
-		if (newRuntime != null) {
-			runtimeId = newRuntime.getId();
-			if (save)
-				savePreferences(monitor);
-			IRuntimeType runtimeType = newRuntime.getRuntimeType();
-			for (int i = 0; i < size; i++) {
-				IRuntimeTargetHandler handler = handlers[i];
-				long time = System.currentTimeMillis();
-				boolean supports = handler.supportsRuntimeType(runtimeType);
-				Trace.trace(Trace.RUNTIME_TARGET, "  > " + handler + " " + supports);
-				if (supports)
-					((RuntimeTargetHandler)handler).setRuntimeTarget(project, newRuntime, monitor);
-				Trace.trace(Trace.PERFORMANCE, "Runtime target: <" + (System.currentTimeMillis() - time) + "> " + handler.getId());
-			}
-		} else {
-			runtimeId = null;
-			if (save)
-				savePreferences(monitor);
-		}
-		
-		Trace.trace(Trace.RUNTIME_TARGET, "setRuntimeTarget <");
+		return null;
 	}
 
 	/**
@@ -220,8 +163,8 @@ public class ProjectProperties implements IProjectProperties {
 		serverProject = b;
 		savePreferences(monitor);
 	}
-	
+
 	public String toString() {
-		return "ProjectProperties[" + project + ", " + runtimeId + "]";
+		return "ProjectProperties[" + project + "]";
 	}
 }
