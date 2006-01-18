4cf90aec58ff21a62d9ff683e62a5b6390d34a78
deboer <deboer>
2006-01-18 14:27:28 +0000

    [111082] JUnit Cactus fix

--- plugins/org.eclipse.jst.server.core/sjavacore/org/eclipse/jst/server/core/internal/cactus/CactusModuleArtifactAdapterDelegate.java
+++ plugins/org.eclipse.jst.server.core/sjavacore/org/eclipse/jst/server/core/internal/cactus/CactusModuleArtifactAdapterDelegate.java
@@ -25,6 +25,8 @@ import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.ITypeHierarchy;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.Signature;
 import org.eclipse.wst.server.core.IModule;
 import org.eclipse.wst.server.core.IModuleArtifact;
 import org.eclipse.wst.server.core.ServerUtil;
@@ -35,6 +37,10 @@ import org.eclipse.wst.server.core.model.ModuleArtifactAdapterDelegate;
 public class CactusModuleArtifactAdapterDelegate extends ModuleArtifactAdapterDelegate {
 	private static final String SERVLET_TEST_CASE_TYPE = "org.apache.cactus.ServletTestCase";
 
+	private static final String SUITE_METHOD = "suite";
+
+	private static final String TEST_CLASS_NAME = "junit.framework.Test";
+
 	public IModuleArtifact getModuleArtifact(Object obj) {
 		String methodName = "";
 		if (obj instanceof IMethod) {
@@ -44,12 +50,11 @@ public class CactusModuleArtifactAdapterDelegate extends ModuleArtifactAdapterDe
 		}
 		if (obj instanceof IAdaptable) {
 			IResource resource = (IResource) ((IAdaptable) obj).getAdapter(IResource.class);
-			String testClassName = getClassNameForType(resource,
-					SERVLET_TEST_CASE_TYPE);
+			String testClassName = getClassNameForType(resource, SERVLET_TEST_CASE_TYPE);
 			String projectName = resource.getProject().getName();
 			if (testClassName != null) {
-				return new WebTestableResource(getModule(resource.getProject()),
-						false, projectName, testClassName, methodName);
+				return new WebTestableResource(getModule(resource.getProject()), false,
+						projectName, testClassName, methodName);
 			}
 		}
 		return null;
@@ -63,11 +68,11 @@ public class CactusModuleArtifactAdapterDelegate extends ModuleArtifactAdapterDe
 			IPath path = resource.getFullPath();
 			if (!project.hasNature(JavaCore.NATURE_ID) || path == null)
 				return null;
-			
+
 			IJavaProject javaProject = (IJavaProject) project.getNature(JavaCore.NATURE_ID);
 			if (!javaProject.isOpen())
 				javaProject.open(new NullProgressMonitor());
-			
+
 			// output location may not be on classpath
 			IPath outputPath = javaProject.getOutputLocation();
 			if (outputPath != null
@@ -75,7 +80,7 @@ public class CactusModuleArtifactAdapterDelegate extends ModuleArtifactAdapterDe
 				int count = outputPath.segmentCount();
 				path = path.removeFirstSegments(count);
 			}
-			
+
 			// remove initial part of classpath
 			IClasspathEntry[] classPathEntry = javaProject.getResolvedClasspath(true);
 			if (classPathEntry != null) {
@@ -96,14 +101,30 @@ public class CactusModuleArtifactAdapterDelegate extends ModuleArtifactAdapterDe
 			if (types != null) {
 				int size2 = types.length;
 				for (int i = 0; i < size2; i++) {
-					if (hasSuperclass(types[i], superType))
+					if (hasSuperclass(types[i], superType) || hasSuiteMethod(types[i]))
 						return types[i].getFullyQualifiedName();
 				}
 			}
 			return null;
 		} catch (Exception e) {
+			assert false : "Unexpected exception: " + e;
 			return null;
 		}
+	}
+
+	private static boolean hasSuiteMethod(IType type) throws JavaModelException {
+		IMethod[] methods = type.getMethods();
+		int size = methods.length;
+		for (int i = 0; i < size; i++) {
+			IMethod method = methods[i];
+			if (method.getParameterNames().length == 0 && method.getElementName().equals(SUITE_METHOD)) {
+				String returnType = getFullyQualifiedTypeForMangledType(method.getReturnType(), type);
+				if (TEST_CLASS_NAME.equals(returnType)) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	private static IType[] getTypes(IJavaElement element) {
@@ -143,5 +164,29 @@ public class CactusModuleArtifactAdapterDelegate extends ModuleArtifactAdapterDe
 			}
 		}
 		return null;
+	}
+
+	private static String getFullyQualifiedTypeForMangledType(String type,
+			IType declaringType) throws JavaModelException {
+		type = Signature.toString(type);
+		return getFullyQualifiedTypeForType(type, declaringType);
+	}
+
+	private static String getFullyQualifiedTypeForType(String type, IType declaringType)
+			throws JavaModelException {
+		String[][] resolvedTypes = declaringType.resolveType(type);
+		//TODO: Are there legit cases where this would not be the case? We might need to check
+		//for this and bomb out if the type cannot be unambigiously resolved
+		assert resolvedTypes.length == 1 : "The type cannot be unambigiously resolved. Need to handle this case";
+		String[] resolvedType = resolvedTypes[0];
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0; i < resolvedType.length - 1; i++) {
+			buffer.append(resolvedType[i]);
+			buffer.append('.');
+		}
+
+		buffer.append(resolvedType[resolvedType.length - 1]);
+
+		return buffer.toString();
 	}
 }
