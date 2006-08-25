78e0b3be9588b85d052fb73e211229ea292de8f1
rfrost <rfrost>
2006-08-25 14:58:34 +0000

    [no bug] Fixed copyright

--- plugins/org.eclipse.jst.server.generic.core/src/org/eclipse/jst/server/generic/core/internal/ExternalDebugLaunchConfigurationDelegate.java
+++ plugins/org.eclipse.jst.server.generic.core/src/org/eclipse/jst/server/generic/core/internal/ExternalDebugLaunchConfigurationDelegate.java
@@ -1,15 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2005 BEA Systems, Inc. 
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    rfrost@bea.com
- *    tyip@bea.com
- *    
- *    Based on GenericServerLaunchConfigurationDelegate by Gorkem Ercan
+ *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.jst.server.generic.core.internal;
