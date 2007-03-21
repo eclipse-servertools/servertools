3802b2fa6f1edc13c39f714acf7598901127147d
deboer <deboer>
2007-03-21 03:01:12 +0000

    [85971] Add HTTP server adapter

---
+++ tests/org.eclipse.wst.server.http.core.tests/src/org/eclipse/wst/server/http/core/tests/AllTests.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.server.http.core.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for org.eclipse.wtp.wst.server.http.core.tests");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(ExistenceTest.class);
+		suite.addTestSuite(CreationTestCase.class);
+		//$JUnit-END$
+		return suite;
+	}
+}
