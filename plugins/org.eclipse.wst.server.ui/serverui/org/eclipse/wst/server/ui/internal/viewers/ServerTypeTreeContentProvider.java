03b1f9b3980ce1fbfcc47198916feba2e49d42c9
deboer <deboer>
2007-04-03 18:22:34 +0000

    [180748] Filter servers to those that support remote hosts

--- plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/viewers/ServerTypeTreeContentProvider.java
+++ plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/viewers/ServerTypeTreeContentProvider.java
@@ -74,15 +74,16 @@ public class ServerTypeTreeContentProvider extends AbstractTreeContentProvider {
 		if (runtimeType == null)
 			return false;
 		
-		if (moduleType == null)
-			return true;
-		
-		String moduleTypeId = moduleType.getId();
+		String moduleTypeId = null;
+		if (moduleType != null)
+			moduleTypeId = moduleType.getId();
 		if (includeIncompatibleVersions) {
 			if (!ServerUtil.isSupportedModule(runtimeType.getModuleTypes(), moduleTypeId, null))
 				return false;
 		} else {
-			String moduleVersion = moduleType.getVersion();
+			String moduleVersion = null;
+			if (moduleType != null)
+				moduleVersion = moduleType.getVersion();
 			if (!ServerUtil.isSupportedModule(runtimeType.getModuleTypes(), moduleTypeId, moduleVersion))
 				return false;
 		}
