645c44bd959ee04869860f5fb5cc3f22ecdda62e
deboer <deboer>
2007-05-08 01:37:08 +0000

    [185589] Fix copyrights

--- tests/org.eclipse.wst.server.ui.tests/src/org/eclipse/wst/server/ui/tests/dialog/DialogsTestCase.java
+++ tests/org.eclipse.wst.server.ui.tests/src/org/eclipse/wst/server/ui/tests/dialog/DialogsTestCase.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * Copyright (c) 2004, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     IBM Corporation - Initial API and implementation
  *******************************************************************************/
@@ -33,5 +33,4 @@ public class DialogsTestCase extends TestCase {
 		TerminationDialog td = new TerminationDialog(getShell(), "MyServer with a really long name");
 		UITestHelper.assertDialog(td);
 	}
-
 }
