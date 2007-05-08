e313f46dd51530b4c1135d875a3f6e736dc94843
deboer <deboer>
2007-05-08 01:27:08 +0000

    [185589] Fix copyrights

--- plugins/org.eclipse.jst.server.core/sjavacore/org/eclipse/jst/server/core/internal/RuntimeBridge.java
+++ plugins/org.eclipse.jst.server.core/sjavacore/org/eclipse/jst/server/core/internal/RuntimeBridge.java
@@ -1,15 +1,14 @@
-/******************************************************************************
- * Copyright (c) 2005 BEA Systems, Inc.
- * Copyright (c) 2006 IBM Corporation and others.
+/*******************************************************************************
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *    Konstantin Komissarchik - initial API and implementation
  *    IBM Corporation - Support for all server types
- ******************************************************************************/
+ *******************************************************************************/
 package org.eclipse.jst.server.core.internal;
 
 import java.util.ArrayList;
