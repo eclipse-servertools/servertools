895933952b0dd066f0db16dd9b7b7785bde3f89e
deboer <deboer>
2007-05-09 15:56:40 +0000

    [182654] Separate preview server into own plugin

---
+++ plugins/org.eclipse.wst.server.preview.adapter/src/org/eclipse/wst/server/preview/internal/core/Messages.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.server.preview.internal.core;
+
+import org.eclipse.osgi.util.NLS;
+/**
+ * Translated messages.
+ */
+public class Messages extends NLS {
+	public static String canModifyModules;
+	public static String httpPort;
+
+	public static String errorPortInUse;
+	public static String errorPublish;
+
+	static {
+		NLS.initializeMessages(PreviewPlugin.PLUGIN_ID + ".internal.Messages", Messages.class);
+	}
+}
---
+++ plugins/org.eclipse.wst.server.preview.adapter/src/org/eclipse/wst/server/preview/internal/core/PreviewLaunchableAdapterDelegate.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.server.preview.internal.core;
+
+import java.net.URL;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.wst.server.core.IModuleArtifact;
+import org.eclipse.wst.server.core.IServer;
+import org.eclipse.wst.server.core.model.LaunchableAdapterDelegate;
+import org.eclipse.wst.server.core.util.HttpLaunchable;
+import org.eclipse.wst.server.core.util.WebResource;
+
+public class PreviewLaunchableAdapterDelegate extends LaunchableAdapterDelegate {
+	/*
+	 * @see LaunchableAdapterDelegate#getLaunchable(IServer, IModuleArtifact)
+	 */
+	public Object getLaunchable(IServer server, IModuleArtifact moduleArtifact) throws CoreException {
+		if (server == null || moduleArtifact == null)
+			return null;
+		
+		PreviewServer server2 = (PreviewServer) server.loadAdapter(PreviewServer.class, null);
+		if (server2 == null)
+			return null;
+		
+		try {
+			URL url = server2.getModuleRootURL(moduleArtifact.getModule());
+			
+			if (moduleArtifact instanceof WebResource) {
+				WebResource resource = (WebResource) moduleArtifact;
+				String path = resource.getPath().toString();
+				
+				if (path.startsWith("/"))
+					path = path.substring(1);
+				url = new URL(url.toExternalForm() + "/" + path);
+			}
+			return new HttpLaunchable(url);
+		} catch (Exception e) {
+			Trace.trace(Trace.SEVERE, "Error in launchable adapter", e);
+		}
+		
+		return null;
+	}
+}
