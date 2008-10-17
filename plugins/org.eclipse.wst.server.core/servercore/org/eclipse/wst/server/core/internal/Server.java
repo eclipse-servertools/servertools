a000a506f25e9bb1e19a692f37c3b197f20a6fc2
deboer <deboer>
2008-10-17 14:47:53 +0000

    [251187] NPE on publish

--- plugins/org.eclipse.wst.server.core/servercore/org/eclipse/wst/server/core/internal/Server.java
+++ plugins/org.eclipse.wst.server.core/servercore/org/eclipse/wst/server/core/internal/Server.java
@@ -1123,7 +1123,7 @@ public class Server extends Base implements IServer {
 		
 		// to make the publishing dialog appear, require adaptable to covert String.class
 		// into "user" string. a bit of a kludge, but works fine for now
-		if (info != null && info.getAdapter(String.class).equals("user"))
+		if (info != null && "user".equals(info.getAdapter(String.class)))
 			publishJob.setUser(true);
 		publishJob.schedule();
 	}
