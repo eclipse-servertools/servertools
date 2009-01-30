f1a22b61bd15b2f7c50bcbdcc00fd441fee1fb7b
avera <avera>
2009-01-30 05:08:06 +0000

    [262997] Need to adapt to platform changes on bug# 252303

--- plugins/org.eclipse.jst.server.generic.core/src/org/eclipse/jst/server/generic/internal/xml/XMLUtils.java
+++ plugins/org.eclipse.jst.server.generic.core/src/org/eclipse/jst/server/generic/internal/xml/XMLUtils.java
@@ -1,5 +1,5 @@
 /***************************************************************************************************
- * Copyright (c) 2005 Eteration A.S. and Gorkem Ercan. All rights reserved. This program and the
+ * Copyright (c) 2005,2009 Eteration A.S. and Gorkem Ercan. All rights reserved. This program and the
  * accompanying materials are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
@@ -114,10 +114,11 @@ public class XMLUtils {
             return null;
         }
 		try {
-			java.net.URI uri = new java.net.URI(url.getProtocol(), url.getHost(),url.getPath(), url.getQuery());
+			java.net.URI uri = new java.net.URI(url.toExternalForm());
 		    return uri;
 		} catch (URISyntaxException e) {
-			//ignore
+			CorePlugin.getDefault().getLog().log( new Status(IStatus.ERROR, CorePlugin.PLUGIN_ID, 1,
+                            "Error loading the definition file", e)); //$NON-NLS-1$
 		}
         return null;
     }
