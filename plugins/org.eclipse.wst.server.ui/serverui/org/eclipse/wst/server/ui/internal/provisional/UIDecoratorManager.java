a270663b150172693c3f0af19de2b4c803e96a9b
avera <avera>
2009-02-24 18:11:08 +0000

    [265974] ServersView2: No label provider loaded [260586] ServersView2: Incorrect package for new servers view

--- plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/provisional/UIDecoratorManager.java
+++ plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/provisional/UIDecoratorManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005,2008 IBM Corporation and others.
+ * Copyright (c) 2005,2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,7 +11,7 @@
 package org.eclipse.wst.server.ui.internal.provisional;
 
 import org.eclipse.wst.server.core.IServerType;
-import org.elcipse.wst.server.ui.internal.cnf.CNFManagedUIDecorator;
+import org.eclipse.wst.server.ui.internal.cnf.CNFManagedUIDecorator;
 
 public class UIDecoratorManager {
 	protected static UIDecorator decorator = new ManagedUIDecorator();
