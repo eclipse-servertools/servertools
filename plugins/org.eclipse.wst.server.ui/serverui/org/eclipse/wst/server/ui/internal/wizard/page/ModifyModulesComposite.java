9a62caecf9150ed7c227e2a122c27ba4cc385913
avera <avera>
2009-12-01 21:55:08 +0000

    [296591] Layout problem on Add/Remove module dialog

--- plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/wizard/page/ModifyModulesComposite.java
+++ plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/wizard/page/ModifyModulesComposite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2007 IBM Corporation and others.
+ * Copyright (c) 2003, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -522,7 +522,7 @@ public class ModifyModulesComposite extends Composite {
 		
 		// slosh buttons
 		Composite comp = new Composite(this, SWT.NONE);
-		data = new GridData(GridData.FILL_BOTH);
+		data = new GridData(GridData.FILL_VERTICAL | GridData.HORIZONTAL_ALIGN_FILL);
 		data.widthHint = 120;
 		comp.setLayoutData(data);
 		
