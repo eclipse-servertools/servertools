a8ec0949d6f51b7f2285e043d98b6e486112c38a
avera <avera>
2010-03-08 15:27:09 +0000

    [303516] J2EE preview server doesn't publish app correctly when singleRoot=true

--- plugins/org.eclipse.jst.server.preview.adapter/src/org/eclipse/jst/server/preview/adapter/internal/core/PreviewServerBehaviour.java
+++ plugins/org.eclipse.jst.server.preview.adapter/src/org/eclipse/jst/server/preview/adapter/internal/core/PreviewServerBehaviour.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -176,8 +176,10 @@ public class PreviewServerBehaviour extends ServerBehaviourDelegate {
 	 */
 	protected void publishModule(int kind, int deltaKind, IModule[] moduleTree, IProgressMonitor monitor) throws CoreException {
 		IModule module = moduleTree[moduleTree.length - 1];
-		if (isSingleRootStructure(module))
+		if (isSingleRootStructure(module)) {
+			setModulePublishState(moduleTree, IServer.PUBLISH_STATE_NONE);
 			return;
+		}
 		
 		IPath to = getModulePublishDirectory(module);
 		
