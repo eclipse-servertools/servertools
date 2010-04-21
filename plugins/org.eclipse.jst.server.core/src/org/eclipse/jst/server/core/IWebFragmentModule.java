73b8d2df72194b14b5da21113b4aef6e01aa560e
avera <avera>
2010-04-21 20:08:01 +0000

    [309506] Should IWebFragmentModule exists

---
+++ plugins/org.eclipse.jst.server.core/src/org/eclipse/jst/server/core/IWebFragmentModule.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jst.server.core;
+
+/**
+ * A J2EE web fragment module.
+ * <p>
+ * <b>Provisional API:</b> This class/interface is part of an interim API that is still under development and expected to 
+ * change significantly before reaching stability. It is being made available at this early stage to solicit feedback 
+ * from pioneering adopters on the understanding that any code that uses this API will almost certainly be broken 
+ * (repeatedly) as the API evolves.
+ * </p>
+ * 
+ * @plannedfor 3.0
+ */
+public interface IWebFragmentModule extends IJ2EEModule {
+	// intentionally empty
+}
