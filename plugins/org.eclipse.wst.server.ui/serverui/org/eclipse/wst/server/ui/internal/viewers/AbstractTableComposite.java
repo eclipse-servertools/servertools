20daf3e4d47560405aebb674ba0e3d3623e00478
eyuen <eyuen>
2011-03-11 22:07:14 +0000

    [328311] RuntimePreferencePage does not update if new runtimes are added

--- plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/viewers/AbstractTableComposite.java
+++ plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/viewers/AbstractTableComposite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2007 IBM Corporation and others.
+ * Copyright (c) 2003, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -75,5 +75,9 @@ public abstract class AbstractTableComposite extends Composite {
 
 	public void remove(Object obj) {
 		tableViewer.remove(obj);
+	}
+	
+	public void add(Object obj) {
+		tableViewer.add(obj);
 	}
 }
