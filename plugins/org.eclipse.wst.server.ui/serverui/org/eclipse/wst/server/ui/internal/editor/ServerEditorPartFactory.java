0aef8d55d84330fe04256bedc61e7212a4903805
avera <avera>
2011-04-19 15:19:00 +0000

    [337349] org.eclipse.wst.server.ui.editorPageSection property tester not called on first open

--- plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/editor/ServerEditorPartFactory.java
+++ plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/editor/ServerEditorPartFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2007 IBM Corporation and others.
+ * Copyright (c) 2003, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -237,6 +237,7 @@ public class ServerEditorPartFactory implements IServerEditorPartFactory {
 		if (getContextualLaunchEnablementExpression() == null)
 			return true;
 		IEvaluationContext context = new EvaluationContext(null, obj);
+		context.setAllowPluginActivation(true);
 		context.addVariable("server", obj);
 		return evalEnablementExpression(context, getContextualLaunchEnablementExpression());
 	}
