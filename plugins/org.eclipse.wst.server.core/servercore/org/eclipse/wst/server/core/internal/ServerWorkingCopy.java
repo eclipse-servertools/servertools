588b1c43b26740358a0d8bb3dd5ae5be87abab30
eyuen <eyuen>
2011-08-03 21:47:48 +0000

    [352001] Intermittent problem when changing server hostname in server editor can mess up sever status

--- plugins/org.eclipse.wst.server.core/servercore/org/eclipse/wst/server/core/internal/ServerWorkingCopy.java
+++ plugins/org.eclipse.wst.server.core/servercore/org/eclipse/wst/server/core/internal/ServerWorkingCopy.java
@@ -447,6 +447,9 @@ public class ServerWorkingCopy extends Server implements IServerWorkingCopy {
 			getWorkingCopyDelegate(monitor).saveConfiguration(monitor);
 		wch.setDirty(false);
 		
+		if (getServerState() == IServer.STATE_STARTED)
+			autoPublish();
+		
 		return server;
 	}
 
