dfc57b8ff314278ce0463cec9b26349851765004
eyuen <eyuen@ca.ibm.com>
2013-11-28 11:52:10 -0500

    [413506] restart module action has no icons

--- plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/view/servers/RestartModuleAction.java
+++ plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/view/servers/RestartModuleAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2010 IBM Corporation and others.
+ * Copyright (c) 2003, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,6 +13,7 @@ package org.eclipse.wst.server.ui.internal.view.servers;
 import org.eclipse.jface.action.Action;
 import org.eclipse.wst.server.core.IModule;
 import org.eclipse.wst.server.core.IServer;
+import org.eclipse.wst.server.ui.internal.ImageResource;
 import org.eclipse.wst.server.ui.internal.Messages;
 /**
  * Restart a module on a server.
@@ -27,6 +28,9 @@ public class RestartModuleAction extends Action {
 		this.module = module;
 		
 		setText(Messages.actionRestartModule);
+		setImageDescriptor(ImageResource.getImageDescriptor(ImageResource.IMG_ELCL_RESTART));
+		setHoverImageDescriptor(ImageResource.getImageDescriptor(ImageResource.IMG_CLCL_RESTART));
+		setDisabledImageDescriptor(ImageResource.getImageDescriptor(ImageResource.IMG_DLCL_RESTART));
 		
 		setEnabled(server.getServerState() == IServer.STATE_STARTED
 				&& server.getModuleState(module) != IServer.STATE_UNKNOWN
