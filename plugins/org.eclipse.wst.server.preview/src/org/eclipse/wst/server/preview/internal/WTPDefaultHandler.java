66f16e95b0b73472301cc4604bd81997c7f2d6bb
Steven Hung <sghung@ca.ibm.com>
2014-11-11 15:27:26 -0500

    [450015] Jetty 9 is shipped in Eclipse Mars

--- plugins/org.eclipse.wst.server.preview/src/org/eclipse/wst/server/preview/internal/WTPDefaultHandler.java
+++ plugins/org.eclipse.wst.server.preview/src/org/eclipse/wst/server/preview/internal/WTPDefaultHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008,2011 IBM Corporation and others.
+ * Copyright (c) 2008,2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -39,7 +39,7 @@ public class WTPDefaultHandler extends AbstractHandler {
 		baseRequest.setHandled(true);
 		
 		response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-		response.setContentType(MimeTypes.TEXT_HTML);
+		response.setContentType(MimeTypes.Type.TEXT_HTML.toString());
 		
 		ByteArrayISO8859Writer writer = new ByteArrayISO8859Writer(1500);
 		
