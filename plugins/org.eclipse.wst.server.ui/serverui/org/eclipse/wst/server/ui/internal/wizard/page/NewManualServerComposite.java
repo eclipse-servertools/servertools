db9bd95482121553c7eb8f2425c33389ee9138e2
Steven Hung <sghung@ca.ibm.com>
2016-01-08 15:32:26 -0500

    [483526] New Server Wizard - server type does not execute handlePropertyChanged correctly

--- plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/wizard/page/NewManualServerComposite.java
+++ plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/wizard/page/NewManualServerComposite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2015 IBM Corporation and others.
+ * Copyright (c) 2003, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -815,7 +815,7 @@ public class NewManualServerComposite extends Composite implements IUIControlLis
 
 			return;
 		}
-		oldServerType = serverType;
+
 		if (serverType != null){
 			// Update the old server type value.
 			hostname.setVisible(true);
