bec31d18973b5bdb5c9801008ec5c7eae867c1c4
sghung@ca.ibm.com <sghung@ca.ibm.com>
2016-03-23 18:16:06 -0400

    [488796] Unnecessary TaskModel updates in New Server Wizard - revert changes to fix breakage

--- plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/wizard/page/NewManualServerComposite.java
+++ plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/wizard/page/NewManualServerComposite.java
@@ -937,12 +937,16 @@ public class NewManualServerComposite extends Composite implements IUIControlLis
 	protected void hostnameChanged(String newHost) {
 		if (newHost == null)
 			return;
+		/*
+		 * Bug 349434, with the fix in Timer.runTimer, the chance that a new 
+		 * host name is the same as the host name will be very rare. In some  
+		 * cases, it still needs to go through processes such as loadServerImpl. 
+		 * It doesn't worth to handle it differently. Therefore, we are not checking 
+		 * for the same host name in here.
+		 */
 
-		String oldHost = host;
-		if (oldHost!= null && !oldHost.equalsIgnoreCase(newHost)){
-			host = newHost;
-			hostnameListener.hostnameSelected(host);
-		}
+		host = newHost;
+		hostnameListener.hostnameSelected(host);
 	}
 
 	public void handleUIControlMapChanged(Map<String, UIControlEntry> controlMap) {
