c4ceed65855920f00f81ff844ee1db04f69aada7
sghung@ca.ibm.com <sghung@ca.ibm.com>
2016-08-23 18:28:17 -0400

    [499865] A warning or confirmation dialog should appear when modules are removed from a server

--- tests/org.eclipse.wst.server.ui.tests/src/org/eclipse/wst/server/ui/tests/dialog/UITestHelper.java
+++ tests/org.eclipse.wst.server.ui.tests/src/org/eclipse/wst/server/ui/tests/dialog/UITestHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2011 IBM Corporation and others.
+ * Copyright (c) 2004, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -92,7 +92,7 @@ public class UITestHelper {
 		// fill the manager with contributions from the matching contributors
 		PropertyPageContributorManager.getManager().contribute(manager, element);
 		
-		IWorkbenchAdapter adapter = (IWorkbenchAdapter)element.getAdapter(IWorkbenchAdapter.class);
+		IWorkbenchAdapter adapter = element.getAdapter(IWorkbenchAdapter.class);
 		if (adapter != null) {
 			name = adapter.getLabel(element);
 		}
