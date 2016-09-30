e7843bd4076fd713bd188481cd792e31ce6a9e20
eyuen <eyuen@ca.ibm.com>
2016-09-30 14:11:19 -0400

    [496805] Use not deprecated Search field style for FilteredTree
    Change-Id: I21e858e67e443cc41aa1334b16ad6a5e7075f90c

--- plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/viewers/ServerComposite.java
+++ plugins/org.eclipse.wst.server.ui/serverui/org/eclipse/wst/server/ui/internal/viewers/ServerComposite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2015 IBM Corporation and others.
+ * Copyright (c) 2003, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -109,7 +109,7 @@ public class ServerComposite extends AbstractTreeComposite implements IShellProv
 	}
 
 	protected void createTree() {
-		tree = new FilteredTree(this, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL | SWT.SINGLE | SWT.FULL_SELECTION, new ServerPatternFilter());
+		tree = new FilteredTree(this, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL | SWT.SINGLE | SWT.FULL_SELECTION, new ServerPatternFilter(), true);
 	}
 
 	public void setIncludeIncompatibleVersions(boolean b) {
