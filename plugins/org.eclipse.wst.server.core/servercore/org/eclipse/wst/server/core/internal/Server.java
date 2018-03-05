10680bf9e7a9bc05220ca741121f86ec69b851fc
Rob Stryker <stryker@redhat.com>
2018-03-05 11:05:43 -0500

    [532023] - getServerPorts default impl should return empty array
    Signed-off-by: Rob Stryker <stryker@redhat.com>

--- plugins/org.eclipse.wst.server.core/servercore/org/eclipse/wst/server/core/internal/Server.java
+++ plugins/org.eclipse.wst.server.core/servercore/org/eclipse/wst/server/core/internal/Server.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2017 IBM Corporation and others.
+ * Copyright (c) 2003, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -3027,7 +3027,7 @@ public class Server extends Base implements IServer {
 			return getDelegate(monitor).getServerPorts();
 		} catch (Exception e) {
 			ServerPlugin.logExtensionFailure(toString(), e);
-			return null;
+			return new ServerPort[0];
 		}
 	}
 
