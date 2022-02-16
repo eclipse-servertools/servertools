6d02b4043ea30fbdd6f8d4e3f85cc5b993f1b381
Nitin Dahyabhai <thatnitind@gmail.com>
2022-02-16 12:55:09 -0500

    [493277] Update plug-in version and copyrights

--- plugins/org.eclipse.jst.server.tomcat.core/tomcatcore/org/eclipse/jst/server/tomcat/core/internal/Tomcat100PublishModuleVisitor.java
+++ plugins/org.eclipse.jst.server.tomcat.core/tomcatcore/org/eclipse/jst/server/tomcat/core/internal/Tomcat100PublishModuleVisitor.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2020 SAS Institute and others.
+ * Copyright (c) 2020, 2022 SAS Institute and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
