ead2d9ba0c7e97f187ea004c5587b312ab5ceda1
Nitin Dahyabhai <thatnitind@gmail.com>
2023-07-09 23:53:47 -0400

    [nobug] Add some UI descriptions
    Change-Id: I65e37c4bcd9db625ab7436aa360d36873bf749f7

--- plugins/org.eclipse.jst.server.tomcat.ui/tomcatui/org/eclipse/jst/server/tomcat/ui/internal/TomcatRuntimeComposite.java
+++ plugins/org.eclipse.jst.server.tomcat.ui/tomcatui/org/eclipse/jst/server/tomcat/ui/internal/TomcatRuntimeComposite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2021 IBM Corporation and others.
+ * Copyright (c) 2003, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -276,6 +276,7 @@ public class TomcatRuntimeComposite extends Composite {
 		label.setLayoutData(data);
 		
 		combo = new Combo(this, SWT.DROP_DOWN | SWT.READ_ONLY);
+		combo.setToolTipText(Messages.installedJREdescription);
 		combo.setItems(jreNames);
 		data = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
 		combo.setLayoutData(data);
